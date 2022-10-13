2208ebede93400cc48936e42c792e762aa15b1ed
grazirs <graziribeiro95@gmail.com>
2022-10-13 17:26:16 -0300

    update components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
   children: ReactNode;
   asChild?: boolean;
 }
 
-export const Button = ({ children, asChild}: ButtonProps) => {
+export const Button = ({ children, asChild, className, ...props}: ButtonProps) => {
   const Component = asChild ? Slot : 'button'
   return (
     <Component className={clsx(
